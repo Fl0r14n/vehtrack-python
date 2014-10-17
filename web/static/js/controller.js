e7282922cf73bd5bad186524e9e8cd16bb91a1cb
Florian Chis <florian.chis@3pillarglobal.com>
2014-10-17 18:40:43 +0300

    login popup

--- web/static/js/controller.js
+++ web/static/js/controller.js
@@ -2,9 +2,38 @@
 
 var controllers = angular.module('controller', ['service']);
 
-controllers.controller('MainController', function($scope) {
+controllers.controller('MainController', [function($scope) {
+
+}]);
+
+controllers.controller('LoginController', function($modal) {
+
+    this.open = function(size) {
+        var modalWindow = $modal.open({
+            templateUrl: 'login_modal.html',
+            size: size,
+            controller: function($scope, $modalInstance) {
+                $scope.login = function() {
+                    //TODO login
+                    //pass the user to close method
+                    $modalInstance.close();
+                };
+
+                $scope.cancel = function() {
+                    $modalInstance.dismiss('cancel');
+                }
+            }
+        });
+
+        modalWindow.result.then(function() {
+
+        }, function() {
+            //dismiss modal
+        })
+    };
 
 });
+
 
 controllers.controller('DeviceController', ['DeviceService', function(deviceService) {
     var self = this;
